ec6325409fe3ea721477e3df2b42aa5c84819894
omar <omark012000@gmail.com>
2024-11-20 05:41:07 -0500

    added community info to admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Community
+
 
 # Register your models here.
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "list_members",
+    )  # Customize columns
+    search_fields = ("name", "description")  # Add search functionality
+
+    def list_members(self, obj):
+        return ", ".join([member.username for member in obj.members.all()])
+
+    list_members.short_description = "Members"
+
+    list_filter = ("created_at",)  # Add filtering options
