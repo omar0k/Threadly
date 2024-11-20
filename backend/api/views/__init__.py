0a159ed07904d04f1e90d057ffb6cdb729b44059
omar <omark012000@gmail.com>
2024-11-20 05:43:19 -0500

    updated community views

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -1,4 +1,10 @@
-from .community_views import CommunityListCreateView, CommunityDeleteView,JoinCommunity
-from .note_views import NoteListCreateView, NoteDeleteView
+from .community_views import (
+    CommunityListCreateView,
+    CommunityDeleteView,
+    JoinCommunity,
+    LeaveCommunity,
+    CommunityThreadsView,
+)
+from .thread_views import ThreadListCreateView, ThreadDeleteView
 from .user_views import CreateUserView
 from .hello_view import hello_world
--- backend/api/views/community_views.py
+++ backend/api/views/community_views.py
@@ -1,6 +1,7 @@
+from urllib import response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from ..models import Community
+from ..models import Community, Thread
 from ..serializers import CommunitySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -18,24 +19,51 @@ class CommunityListCreateView(generics.ListCreateAPIView):
         community.members.add(self.request.user)
 
 
+class CommunityThreadsView(generics.ListAPIView):
+    serializer_class = CommunitySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        print(self.kwargs)
+        community_id = self.kwargs.get("community_id")
+        try:
+            community = Community.objects.get(id=community_id)
+        except Community.DoesNotExist:
+            return Response({"error": "Community not found."})
+        return Thread.objects.filter(community=community)
+
+
+class LeaveCommunity(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, community_id):
+        try:
+
+            community = Community.objects.get(id=community_id)
+            if request.user not in community.members.all():
+                return Response(
+                    {"message": "User not a member of this community."}, status=400
+                )
+            community.members.remove(request.user)
+            return Response({"message": "Successfully left community."}, status=200)
+        except Community.DoesNotExist:
+            return Response({"error": "Community not found"}, status=404)
+
+
 class JoinCommunity(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, community_id):
         try:
-            # Get the community by ID
             community = Community.objects.get(id=community_id)
-            # Check if user is already in community
             if request.user in community.members.all():
                 return Response(
                     {"message": "You are already a member of this community."},
                     status=400,
                 )
-            # Add the logged-in user to the community
             community.members.add(request.user)
-
             return Response(
-                {"message": "Successfully joined the community"}, status=200
+                {"message": "Successfully joined the community."}, status=200
             )
         except Community.DoesNotExist:
             return Response({"error": "Community not found"}, status=404)
