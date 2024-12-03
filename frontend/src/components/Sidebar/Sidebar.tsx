648a77ca2aa7d174aa7b19c8c389bdf0706f5fc5
omar <omark012000@gmail.com>
2024-12-02 23:52:49 -0500

    refactored sidebar components

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,24 +1,32 @@
-import { Community } from '@/types';
-import React, { FC } from 'react';
-import SidebarTab from './SidebarTab';
-import { PlusIcon } from 'lucide-react';
+import { Community } from "@/types";
+import React, { useEffect, useState } from "react";
+import SidebarTab from "./SidebarTab";
+import { PlusIcon } from "lucide-react";
+import { CreateCommunity } from "../CreateCommunity";
+import api from "@/api";
 interface SidebarProps {
   communities?: Community[];
 }
-const Sidebar: React.FC<SidebarProps> = ({ communities }) => {
+const Sidebar: React.FC<SidebarProps> = () => {
+  const [communities, setCommunities] = useState<Community[]>([]);
+  useEffect(() => {
+    const fetchCommunities = async () => {
+      const response = await api.get("/api/communities/");
+
+      console.log(response);
+      setCommunities(response.data);
+    };
+    fetchCommunities();
+  }, []);
   return (
-    <div className='basis-64 p-5'>
-      <SidebarTab
-        isCommunity={false}
-        tabIcon={<PlusIcon size={32} />}
-        tabText='Create a community'
+    <div className="basis-64 p-5">
+      <CreateCommunity
+        onCommunityCreated={(newCommunity) =>
+          setCommunities((prev) => [...prev, newCommunity])
+        }
       />
       {communities?.map((community) => (
-        <SidebarTab
-          key={community.id}
-          community={community}
-          isCommunity={true}
-        />
+        <SidebarTab key={community.id} community={community} />
       ))}
     </div>
   );
--- frontend/src/components/Sidebar/SidebarTab.tsx
+++ frontend/src/components/Sidebar/SidebarTab.tsx
@@ -1,39 +1,24 @@
-import React from 'react';
-import { Community } from '@/types';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Community } from "@/types";
+import { useNavigate } from "react-router-dom";
 interface CommunitySideTabProps {
-  community?: Community; // Type the community prop
-  isCommunity: boolean;
-  tabText?: string;
-  tabIcon?: React.ReactNode;
-  tabLink?: string;
+  community: Community;
 }
-const SidebarTab: React.FC<CommunitySideTabProps> = ({
-  community,
-  isCommunity,
-  tabIcon,
-  tabText,
-}) => {
+const SidebarTab: React.FC<CommunitySideTabProps> = ({ community }) => {
   const navigate = useNavigate();
-  if (!isCommunity) {
-    return (
-      <div
-        onClick={() => navigate('/create-community')}
-        className='hover:cursor-pointer flex items-center gap-3 rounded-md  hover:bg-gray-800 p-1 hover:text-white transition-colors duration-200'
-      >
-        {tabIcon && <span>{tabIcon}</span>}
-        <span>{tabText}</span>
-      </div>
-    );
-  }
   return (
-    <div className='hover:cursor-pointer flex items-center gap-3 rounded-md  hover:bg-gray-800 p-1 hover:text-white transition-colors duration-200'>
+    <div
+      onClick={() =>
+        navigate(`${community.name}`, { state: { id: community.id } })
+      }
+      className="hover:cursor-pointer flex items-center gap-3 rounded-md  hover:bg-gray-800 p-1 hover:text-white transition-colors duration-200"
+    >
       <img
-        className='w-8 h-8 object-cover rounded-full'
+        className="w-8 h-8 object-cover rounded-full"
         src={
           community?.image_url
             ? import.meta.env.VITE_API_URL + community?.image_url
-            : 'https://placehold.co/400'
+            : "https://placehold.co/400"
         }
         alt={community?.name}
       />
